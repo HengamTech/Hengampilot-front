9676d54da2cd125909ae854399fb422c15f934c0
alirezabg18 <abgo44@gmail.com>
2024-12-11 15:12:26 +0330

    updating category

--- frontend/src/components/category/category.js
+++ frontend/src/components/category/category.js
@@ -5,71 +5,65 @@ const categories = [
   {
     icon: "🍴",
     text: "غذا، رستوران، کافه",
-    link: "/foods",
   },
   {
     icon: "⚽",
     text: "ورزش",
-    link: "/sports",
   },
   {
     icon: "📺",
     text: "لوازم منزل",
-    link: "/furnishings",
   },
   {
     icon: "🎓",
     text: "آموزشی",
-    link: "education",
   },
   {
     icon: "🏠",
     text: "خدمات منزل",
-    link: "/Home-services", // Add a link for "خدمات منزل"
+    link: "/Home" // Add a link for "خدمات منزل"
   },
-  {
-    icon: "✈️",
-    text: "خدمات مسافرتی",
-    link: "travel-services",
-  },
+
+];
+const categories2 = [
   {
     icon: "⚖️",
     text: "خدمات حقوقی",
-    link: "Legal-services",
   },
   {
     icon: "📰",
     text: "رسانه و اخبار",
-    link: "news-media",
   },
   {
     icon: "💰",
     text: "خدمات مالی",
-    link: "Financial-services",
   },
   {
     icon: "🚶",
     text: "خدمات عمومی",
-    link: "general-services",
+  },
+  {
+    icon: "✈️",
+    text: "خدمات مسافرتی",
   },
 
-];
 
+]
 
 const Categories = () => {
-
+  
     const handleClick = (link) => {
       if (link) {
         window.location.href = link; // هدایت به سایت خارجی
       }
     };
   return (
-
+    
     <div className="container mt-4">
       <h2 className="text-center mb-5">دسته‌بندی‌ها</h2>
       <div className="row text-center">
         {categories.map((category, index) => (
-          <div className="category-card col-6 col-md-2 mb-3" key={index}  onClick={() => handleClick(category.link)} >
+          <div className="category-card col-5 col-lg-2 justify-content-center" key={index}  onClick={() => handleClick(category.link)} > 
             <div className="category-card-inner shadow p-3 mb-3 bg-white rounded h-100 hover-scale">
               <span
                 className="fs-1 mb-2"
@@ -83,6 +77,24 @@ const Categories = () => {
           </div>
         ))}
       </div>
+      <div className="row text-center">
+        {categories2.map((category2, index) => (
+          <div className="category-card col-5 col-lg-2 justify-content-center" key={index}  onClick={() => handleClick(category2.link)} > 
+            <div className="category-card-inner shadow p-3 mb-3 bg-white rounded h-100 hover-scale">
+              <span
+                className="fs-1 mb-2"
+                role="img"
+                aria-label={category2.text}
+              >
+                {category2.icon}
+              </span>
+              <p className="m-0">{category2.text}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+
+
     </div>
   );
 };
