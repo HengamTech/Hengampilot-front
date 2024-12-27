e6e32a3df702984f1820d12de89a083339e80618
alirezabg18 <abgo44@gmail.com>
2024-12-27 19:26:50 +0330

    renaming company for mohammad

--- frontend/src/components/Companylistbycategory2/CompanylistbyCategory3.js
+++ frontend/src/components/Companylistbycategory2/CompanylistbyCategory3.js
@@ -4,7 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { FaStar, FaStarHalfAlt, FaRegStar } from "react-icons/fa";
 import { useNavigate, useParams } from "react-router-dom";
 
-const Companylistbycategory = () => {
+const Companylistbycategory1 = () => {
   // گرفتن categoryId از پارامترهای آدرس
   // مثال: اگر مسیر شما "/category/5" باشد، categoryId = "5"
   const { id: categoryId } = useParams();
@@ -304,4 +304,4 @@ const Companylistbycategory = () => {
   );
 };
 
-export default Companylistbycategory;
+export default Companylistbycategory1;
