2d2e658820e15e8e686e8559a73b97c94e767e58
Mohammad jalilinia <jaliliniaw2001@gmail.com>
2025-01-02 13:11:58 +0330

    footer adjust and update

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -9,32 +9,35 @@ const Footer = () => {
             <div className="footer-content">
                 <div className="footer-section">
                     <div className="footer-logo">
-                        <img src={Logo} alt="Logo" />
-                        <h4>HengamPilot</h4>
+                        <img src={Logo} alt="Logo"/>
+                        <a href="/"><h4>Hengam pilot</h4></a>
                     </div>
+                    <ul>
+                        <li></li>
+                    </ul>
+
                 </div>
                 <div className="footer-section">
                     <h4>انجمن</h4>
                     <ul>
-                        <li>اطمینان به بازخورد</li>
-                        <li>ورود</li>
-                        <li>ثبت نام</li>
+                        <li><a href="/trust"> اطمینان به بازخورد</a></li>
+                        <li><a href="login">ورود</a></li>
+                        <li><a href="/signup">ثبت نام</a></li>
                     </ul>
                 </div>
                 <div className="footer-section">
                     <h4>کسب و کار</h4>
                     <ul>
-                        <li>HengamPilot</li>
-                        <li>وبلاگ برای کسب و کار</li>
+                        <li><a href="/">HengamPilot</a></li>
+                        <li><a href="/login">ورود برای کسب و کار</a></li>
                     </ul>
                 </div>
                 <div className="footer-section">
                     <h4>درباره ما</h4>
                     <ul>
-                        <li>درباره ما</li>
-                        <li>ارتباط با ما</li>
-                        <li>وبلاگ</li>
-                        <li>چگونه دستیار هنگام کار می کند</li>
+                        <li><a href="/aboutus">درباره ما</a></li>
+                        <li><a href="/contactUs">ارتباط با ما</a></li>
+                        <li><a href="/">چگونه دستیار هنگام کار می کند</a></li>
                     </ul>
                 </div>
             </div>
@@ -47,6 +50,8 @@ const Footer = () => {
                 <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
                 <a href="mailto:info@hengampilot.com" target="_blank" rel="noopener noreferrer"><FaEnvelope /></a>
             </div>
+
+            <h5>تمامی حقوق این سایت متعلق به هنگام پایلت است </h5>
         </footer>
     );
 };
